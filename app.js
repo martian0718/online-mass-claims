429dd88513f9d3675615a73e3a1a2cc1d2c6a80b
Martin Wu <martian0718@gmail.com>
2020-01-20 21:06:57 +0000

    added ejs and express

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('app is connected');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res){
+	res.send("Online Mass Claims");
+});
+
+app.get('/output' function(req,res){
+	res.send("output will be here");
+});
+
+app.listen(3000, function(){
+	console.log('app is connected');
+});
