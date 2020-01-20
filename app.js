ed1d16b25466554a19eec8726ee2a90f314ce4e7
Martin Wu <martian0718@gmail.com>
2020-01-20 22:13:04 +0000

    added ejs files and connected mysql

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
+
+app.set('view engine', 'ejs');
+
+var connection = mysql.createConnection({
+	host	: 'localhost',
+	user	: 'root',
+	database: 'mass_claims'
+});
+
 
 app.get('/', function(req,res){
-	res.send("Online Mass Claims");
+	var q = "SELECT COUNT(*) AS count FROM silicosisData";
+	connection.query(q, function(err,results){
+		if(err) throw err;
+		var count = results[0].count;
+		//res.send("We have " + count + " people in our database");
+		//res.send("Online Mass Claims");
+		res.render("landing");
+	});
+	
 });
 
-app.get('/output' function(req,res){
-	res.send("output will be here");
-});
 
 app.listen(3000, function(){
 	console.log('app is connected');
